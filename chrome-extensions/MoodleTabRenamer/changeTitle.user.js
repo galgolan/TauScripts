78ed26f80e6a9d1e4746e155a43c42023daae617
galgolan <golan.gal@gmail.com>
2014-10-25 00:32:00 +0300

    MoodleTabRenamer

--- chrome-extensions/MoodleTabRenamer/changeTitle.user.js
+++ chrome-extensions/MoodleTabRenamer/changeTitle.user.js
@@ -1,48 +1,69 @@
 // ==UserScript==
-// @name Chrome Tab Auto Renamer
+// @name Moodle Tab Renamer
 // @author Galgo
 // @version	1.2
 // @match http://moodle.tau.ac.il/pluginfile.php/*.pdf
 // ==/UserScript==
 
 debug_mode = false;
+show_extension = false;
+clean_filename = false;
 
-chrome.storage.sync.get({
-    debugMode: true
-  }, function(items) {
-    debug_mode = items.debugMode;
-  });
-
+// Extracts filename and extension from URL
 function getFilename()
 {
 	var url = String(window.location);
-	// TODO: extract the filename from the URL
-	var re = new RegExp(/.+:\/\/.+\/(.+\..+)/);
+	// extract the filename from the URL
+	var re = new RegExp(/.+:\/\/.+\/(.+)\.(.+)/);
 	var matches = re.exec(url);
-	if(matches.length != 2) return null;
+	if(matches.length != 3) return null;	// TODO: throw an exception so it would be visible in console
 
-	return matches[1];
+	if(show_extension) return matches[1] + "." + matches[2];
+	else return matches[1];
 }
 
+// Gets the filename without extension
+// Returns the celan filename
+function cleanFilename(filename) {
+	return filename.replace(/\_/g, " ").replace(/-/g, " ");
+}
+
+// Sets the window/tab title
 function changeTitle(newTitle) {
 	document.title = decodeURIComponent(newTitle);
 }
 
+function loadSettings() {
+	chrome.storage.sync.get({
+    	debugMode: true,
+    	showExtension: false,
+    	cleanFilename: false
+  	}, function(items) {
+    	debug_mode = items.debugMode;
+    	show_extension = items.showExtension;
+    	clean_filename = items.cleanFilename;
+  	});
+}
+
 try
 {
+	loadSettings();
+
 	var filename = getFilename();
-	if(filename != null)
+	if(filename != null) {
+		if(clean_filename)
+			filename = cleanFilename(filename);
+
 		changeTitle(filename);
+	}
 }
 catch(x)
 {
-	// Comment this out if you want error messages displayed
 	if(debug_mode) {
 		var err = new Error();
 		err.name = "Unhandled exception in Moodle Tab Renamer";
 		err.message = x.message;
 		err.stack = x.stack || x.stacktrace || "";
-    	//var myStackTrace = x.stack || x.stacktrace || "";
     	throw(err);
 	}
 }
--- chrome-extensions/MoodleTabRenamer/options.js
+++ chrome-extensions/MoodleTabRenamer/options.js
@@ -1,8 +1,13 @@
 // Saves options to chrome.storage
 function save_options() {  
   var debugMode = document.getElementById('debug').checked;
+  var showExtension = document.getElementById('show_ext').checked;
+  var cleanFilename = document.getElementById('clean_filename').checked;
+  
   chrome.storage.sync.set({
-    debugMode: debugMode
+    debugMode: debugMode,
+    showExtension: showExtension,
+    cleanFilename: cleanFilename
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -18,9 +23,13 @@ function save_options() {
 function restore_options() {
   // Use default value debugMode = true.
   chrome.storage.sync.get({
-    debugMode: true
+    debugMode: true,
+    showExtension: false,
+    cleanFilename: false
   }, function(items) {
     document.getElementById('debug').checked = items.debugMode;
+    document.getElementById('show_ext').checked = items.showExtension;
+    document.getElementById('clean_filename').checked = items.cleanFilename;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
